3c2271efe31d73b6fa0b50f1c545e39b00c16eae
ankitkourav-sift <ankitkourav156@gmail.com>
2026-07-09 14:35:14 +0530

    payment update

--- payment/payment.route.js
+++ payment/payment.route.js
@@ -14,12 +14,11 @@ const razorpay = new Razorpay({
 router.post("/orders", async (req, res) => {
   try {
     const { amount } = req.body;
-
-    const options = {
-      amount: amount * 100,
-      currency: "INR",
-      receipt: "receipt_" + Date.now(),
-    };
+const options = {
+  amount: amount,
+  currency: "INR",
+  receipt: "receipt_" + Date.now(),
+};
 
     const order = await razorpay.orders.create(options);
 
